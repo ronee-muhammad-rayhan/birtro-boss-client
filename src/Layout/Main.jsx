39c304cfefff74d7abab612181235e24830c959b
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-10 19:07:02 +0600

    64-3 Create simple Top Banner react responsive carousel

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../pages/Shared/Navbar";
 import Footer from "../pages/Shared/Footer";
 
 const Main = () => {
