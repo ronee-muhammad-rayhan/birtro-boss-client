cb3e63f97dfaee094303043383362f1ebe0586f8
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-10 18:14:47 +0600

    64-2 Setup routes, layout and footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,6 @@ function App() {
 
   return (
     <>
-      <h1>Bistro Boss Restaurent</h1>
-      <button className="btn btn-active">Default</button>
-      <button className="btn btn-active btn-neutral">Neutral</button>
-      <button className="btn btn-active btn-primary">Primary</button>
-      <button className="btn btn-active btn-secondary">Secondary</button>
-      <button className="btn btn-active btn-accent">Accent</button>
-      <button className="btn btn-active btn-ghost">Ghost</button>
-      <button className="btn btn-active btn-link">Link</button>
     </>
   )
 }
