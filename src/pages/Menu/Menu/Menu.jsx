dff72854ada7b8fba62dc7deed864c596f9bbce0
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-11 14:31:38 +0600

    65-3 Create Custom Hook Usemenu To Load Menu Data

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,16 +1,40 @@
 import { Helmet } from 'react-helmet-async';
 import menuImg2 from '../../../assets/menu/menu-bg2.png'
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg'
+import saladImg from '../../../assets/menu/salad-bg.jpg'
+import soupImg from '../../../assets/menu/soup-bg.jpg'
 import Cover from '../../Shared/Cover/Cover';
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert')
+    const soup = menu.filter(item => item.category === 'soup')
+    const salad = menu.filter(item => item.category === 'salad')
+    const pizza = menu.filter(item => item.category === 'salapizza')
+    const offered = menu.filter(item => item.category === 'offered')
+
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={menuImg2} title={`Our Menu`} shortDescription="Would you like to try a dish?"></Cover>
-
+            {/* main cover */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            {/* offerd menu items */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dessert menu items */}
+            <MenuCategory items={desserts} title="Dessert" img={dessertImg}></MenuCategory>
+            {/* pizza menu items */}
+            <MenuCategory items={pizza} title="Pizza" img={pizzaImg}></MenuCategory>
+            {/* salad menu items */}
+            <MenuCategory items={salad} title="Salad" img={saladImg}></MenuCategory>
+            {/* soup menu items */}
+            <MenuCategory items={soup} title="Soup" img={soupImg}></MenuCategory>
         </div>
     );
 };
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,8 +1,17 @@
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = () => {
+const MenuCategory = ({ items, title, img }) => {
     return (
-        <div>
-
+        <div className="pt-8">
+            {
+                title && <Cover img={img} title={title} shortDescription="Would you like to try a dish?"></Cover>
+            }
+            <div className="grid md:grid-cols-2 gap-10 my-16">
+                {
+                    items.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+                }
+            </div>
         </div>
     );
 };
