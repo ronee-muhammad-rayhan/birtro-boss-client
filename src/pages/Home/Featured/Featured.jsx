03c822213ed7dc4df310d622807b5208999123ee
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-11 10:38:30 +0600

    64-8 Create Parallax and Add Testimonials with react rating

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -5,9 +5,9 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className='featured-item text-white pt-8 my-20'>
+        <div className='featured-item text-white bg-fixed pt-8 my-20'>
             <SectionTitle subHeading="check it out" heading={`Featured Item`}></SectionTitle>
-            <div className='md:flex justify-center items-center pb-20 pt-12 px-36'>
+            <div className='md:flex justify-center items-center pb-20 pt-12 px-36 bg-slate-500 bg-opacity-60'>
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -15,7 +15,7 @@ const Featured = () => {
                     <p>Aug 20, 2029</p>
                     <p className="uppercase">Where can i get some?</p>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad adipisci recusandae commodi debitis est, consequatur officiis aliquid nemo sit molestiae iure ea enim? Qui placeat asperiores quis nobis laboriosam, quae autem nostrum sunt voluptatum error architecto nisi aspernatur porro possimus est, quaerat rerum sapiente sit officiis similique aliquam! Incidunt, libero.</p>
-                    <button className='btn btn-outline'>Order Now</button>
+                    <button className='btn btn-outline border-0 border-b-4 mt-4'>Order Now</button>
                 </div>
             </div>
         </div>
