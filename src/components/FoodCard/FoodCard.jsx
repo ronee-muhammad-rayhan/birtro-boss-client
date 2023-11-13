10a527d659d8124f5ac7f3a7fa816984281d7d7b
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-13 21:25:19 +0600

    67-1 Module Introduction and cart icon setup

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,6 +1,9 @@
 
 const FoodCard = ({ item }) => {
     const { name, image, price, recipe } = item;
+    const handleAddToCart = food => {
+        console.log(food);
+    }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
@@ -9,7 +12,9 @@ const FoodCard = ({ item }) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Card</button>
+                    <button
+                        onClick={() => handleAddToCart(item)}
+                        className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Card</button>
                 </div>
             </div>
         </div>
