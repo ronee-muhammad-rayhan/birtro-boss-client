4cda8e2263cdb9e12a2ec33230921a5cfac71487
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-16 07:34:17 +0600

    68-3 Save User if he doesn’t exists in the database

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -32,7 +32,7 @@ const AuthProvider = ({ children }) => {
 
     const googleSignIn = () => {
         setLoading(true);
-        signInWithPopup(auth, googleProvider)
+        return signInWithPopup(auth, googleProvider)
     }
 
     const logOut = () => {
