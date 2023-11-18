6bbac3416759adcbd85b7406fc82fd9656a486f3
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-18 11:55:39 +0600

    69-1 (Recap) Why we use react-query, axios and react hook form

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, useLocation } from "react-router-dom";
 import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 
-const AdminRoute = (children) => {//TODO:{children}
+const AdminRoute = ({ children }) => {//DONE:{children}
     const { user, loading } = useAuth();
     const [isAdmin, isAdminLoading] = useAdmin();
     const location = useLocation();
@@ -14,8 +14,7 @@ const AdminRoute = (children) => {//TODO:{children}
     if (user && isAdmin) {
         return children;
     }
-    // https://github.com/remix-run/react-router/blob/dev/examples/auth/src/App.tsx
-    return <Navigate to="/login" state={{ from: location }} replace></Navigate>
+    return <Navigate to="/" state={{ from: location }} replace></Navigate>
 };
 
 export default AdminRoute;
