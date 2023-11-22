3f79dd93adc227421a1b75959a6d9e0d02b4a0d4
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-22 13:39:04 +0600

    69-4 Upload image to image hosting server imgbb and get image url

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -1,12 +1,23 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 
 const AddItems = () => {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit } = useForm();
+    const axiosPublic = useAxiosPublic();
 
-    const onSubmit = (data) => {
+    const onSubmit = async (data) => {
         console.log(data)
+        // image upload to imgbb and then get an url
+        const imageFile = { image: data.image[0] };
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: { 'content-type': 'multipart/form-data' }
+        });
+        console.log(res.data);
     }
 
     return (
@@ -31,8 +42,8 @@ const AddItems = () => {
                             <label className="label">
                                 <span className="label-text">Category*</span>
                             </label>
-                            <select {...register("category", { required: true })} className="select select-bordered w-full">
-                                <option disabled selected>Select a category</option>
+                            <select defaultValue={"default"}  {...register("category", { required: true })} className="select select-bordered w-full">
+                                <option disabled value="default">Select a category</option>
                                 <option value="salad">Salad</option>
                                 <option value="pizza">Pizza</option>
                                 <option value="soup">Soup</option>
