282df2051b03f50bfb0db4efe64be6b87ee7fd6a
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-22 20:13:19 +0600

    70-6 Use Confirm Card Payment to Process Payment

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -2,13 +2,16 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
+import useAuth from "../../../hooks/useAuth";
 
 const CheckoutForm = () => {
     const [error, setError] = useState('');
     const [clientSecret, setClientSecret] = useState('');
+    const [transactionId, setTransactionId] = useState('');
     const stripe = useStripe();
     const elements = useElements();
     const axiosSecure = useAxiosSecure();
+    const { user } = useAuth();
     const [cart] = useCart();
 
     const totalPrice = cart.reduce((total, item) => total + item.price, 0)
@@ -57,6 +60,32 @@ const CheckoutForm = () => {
             console.log('payment method', paymentMethod);
             setError('');
         }
+
+        // confirm payment
+        // https://stripe.com/docs/js/payment_intents/confirm_card_payment
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        email: user?.email || 'anonymous',
+                        name: user?.displayName || 'anonymous'
+                    },
+                },
+            },
+        );
+
+        if (confirmError) {
+            console.log('confirm error');
+        }
+        else {
+            console.log('payment intent', paymentIntent);
+            if (paymentIntent.status === 'succeeded') {
+                console.log('transaction id', paymentIntent.id);
+                setTransactionId(paymentIntent.id);
+            }
+        }
     };
 
     return (
@@ -81,6 +110,9 @@ const CheckoutForm = () => {
                 Pay
             </button>
             <p className="text-red-600">{error}</p>
+            {
+                transactionId && <p className="text-green-600">Your transaction id: {transactionId}</p>
+            }
         </form>
     );
 };
