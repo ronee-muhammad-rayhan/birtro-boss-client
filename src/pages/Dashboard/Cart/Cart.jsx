06b0924b85330ddedca3d96e9ee0dc10cc667b04
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-22 16:45:04 +0600

    70-1 Module Overview and Create Payment route

--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import useCart from "../../../hooks/useCart";
 import { FaTrashAlt } from 'react-icons/fa'
 import Swal from 'sweetalert2'
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
     const [cart, refetch] = useCart();
@@ -43,7 +44,14 @@ const Cart = () => {
             <div className="flex justify-evenly mb-8">
                 <h2 className="text-4xl">Items: {cart.length}</h2>
                 <h2 className="text-4xl">Total Price: {totalPrice}</h2>
-                <button className="btn btn-primary">Pay</button>
+                {
+                    cart.length ? <Link to="/dashboard/payment">
+                        <button /* disabled={!cart.length} */ className="btn btn-primary">Pay</button>
+                    </Link>
+                        :
+                        <button disabled className="btn btn-primary">Pay</button>
+
+                }
             </div>
             <div className="overflow-x-auto">
                 <table className="table w-full">
---
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,14 @@
+
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+const Payment = () => {
+    return (
+        <div>
+            < SectionTitle heading="Payment" subHeading="Please pay to eat"></SectionTitle>
+            <div>
+                <h2 className="text-4xl">Tak o pakhi tumi uira uira aso</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Payment;
