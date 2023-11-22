012e6a9692eec57239e638f3ee9b019a0f1ea084
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-22 18:49:41 +0600

    70-4 Display card error and Create Server side payment intents

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,6 +1,8 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
+    const [error, setError] = useState('');
     const stripe = useStripe();
     const elements = useElements();
 
@@ -31,8 +33,10 @@ const CheckoutForm = () => {
 
         if (error) {
             console.log('payment error', error);
+            setError(error.message);
         } else {
             console.log('payment method', paymentMethod);
+            setError('');
         }
     };
 
@@ -57,6 +61,7 @@ const CheckoutForm = () => {
             <button className="btn btn-sm btn-primary my-4" type="submit" disabled={!stripe}>
                 Pay
             </button>
+            <p className="text-red-600">{error}</p>
         </form>
     );
 };
